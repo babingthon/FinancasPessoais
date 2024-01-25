2ed9f3123c495d68bcb988b98a92a302fc0b6736
babingthon <babingthon.bandeira@gmail.com>
2024-01-24 23:29:44 -0300

    subindo o projeto

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 0, 15), category: 'food', title: 'McDonalds', value: 32.50 },
+    { date: new Date(2022, 0, 15), category: 'food', title: 'PitsBurg', value: 50.00 },
+    { date: new Date(2022, 0, 8), category: 'rent', title: 'Aluguel AP', value: 1000 },
+    { date: new Date(2022, 1 , 15), category: 'salary', title: 'Salario COMN', value: 5000 },
+]
